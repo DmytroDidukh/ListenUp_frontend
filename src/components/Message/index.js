c8f4f56a62b97dc24dccf9eed39ae2db1e26b541
Dmytro Didukh <jeriko.outback@gmail.com>
2020-06-04 03:30:58 +0300

    Message component

--- src/components/Message/index.js
+++ src/components/Message/index.js
@@ -1,18 +1,21 @@
 import React from "react";
-
+import formatDistanceToNow from 'date-fns/formatDistanceToNow'
+import classNames from 'classnames'
 
 import './Message.scss';
 
-const Message = ({user, date = 'Yesterday at 9:24PM'}) => (
-    <div className='message'>
-        <div className='message__avatar'>
-            <img src={user.avatar} alt={`${user.fullName} avatar`}/>
-        </div>
+const Message = ({user, date = 'Yesterday at 9:24PM', isMe}) => (
+    <div className={classNames('message', {'message--is-me' : isMe})}>
         <div className='message__content'>
-            <div className='message__bubble'>
-                <p className='message__text'>{user.text}</p>
+            <div className='message__avatar'>
+                <img className='avatar' src={user.avatar} alt={`${user.fullName} avatar`}/>
             </div>
-            <time className='message__date'>{date}</time>
+            <div className='message__info'>
+                <div className='message__bubble'>
+                    <p className='message__text'>{user.text}</p>
+                </div>
+                <time className='message__date'>{formatDistanceToNow(date, {addSuffix: true})}</time>
+            </div>
         </div>
     </div>
 );
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,16 +1,25 @@
 import React from 'react';
+
+
 import {Message} from 'components';
 
 const Home = () => {
-const user = {
-    avatar: 'https://lh3.googleusercontent.com/proxy/PJFLyRN85dh3IRn5_JIsi41VLQV9ymoQzDc9exfGCpQVplDL5Gaqergf3PkmrMZB2jUER2YD5SqwvUnKnsZYQZW4-SpWd84tprj2l1L59EDz',
-    fullName: 'Donald Trump',
-    text: 'Hello world'
-}
+    const date = new Date(2020, 5, 2)
+    const user = {
+        avatar: 'https://lh3.googleusercontent.com/proxy/PJFLyRN85dh3IRn5_JIsi41VLQV9ymoQzDc9exfGCpQVplDL5Gaqergf3PkmrMZB2jUER2YD5SqwvUnKnsZYQZW4-SpWd84tprj2l1L59EDz',
+        fullName: 'Donald Trump',
+        text: 'Wow this is amazing.  I never thought I could earn real money with binary options trading.  I thought everyone on the platform was just scammers who don\'t have human feelings.  They scammed me many times.  Before I get her, be careful when investing.  I know there are real managers, Mr Charles wesley him  real and legitimate manager who will do anything to get your earnings after the exchange.  Him has done it for me now, I don\'t have to worry about her trading, him will put  smile on your face again if you need help, You can connect Him on telegram...'
+    }
+    const user2 = {
+        avatar: ' https://ruinformer.com/uploads/_pages/5193/putin.jpg',
+        fullName: 'Vladimir Putin',
+        text: 'You can connect Him on telegram...'
+    }
 
     return (
         <div>
-            <Message user={user}/>
+            <Message user={user} date={date} isMe/>
+            <Message user={user2} date={date}/>
         </div>
     )
 };
