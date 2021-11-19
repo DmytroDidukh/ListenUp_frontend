a702b662644ecd762b2a60bc39c79d7e9e29a6c2
Dmytro Didukh <jeriko.outback@gmail.com>
2021-11-19 23:06:06 +0200

    chore: prettier

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,31 +1,31 @@
-import React from "react";
-import classNames from "classnames";
-import PropTypes from "prop-types";
-import { Button as BaseButton } from "antd";
+import React from 'react';
+import classNames from 'classnames';
+import PropTypes from 'prop-types';
+import { Button as BaseButton } from 'antd';
 
-import { BUTTON_SIZE } from "../../consts";
+import { BUTTON_SIZE } from '../../consts';
 
-import "./Button.scss";
+import './Button.scss';
 
 const Button = ({ size, className, ...rest }) => {
-  return (
-    <BaseButton
-      {...rest}
-      className={classNames("button", className, {
-        "button--large": size === "large",
-      })}
-    />
-  );
+    return (
+        <BaseButton
+            {...rest}
+            className={classNames('button', className, {
+                'button--large': size === 'large',
+            })}
+        />
+    );
 };
 
 Button.propTypes = {
-  size: PropTypes.oneOf(Object.values(BUTTON_SIZE)),
-  classNames: PropTypes.string,
+    size: PropTypes.oneOf(Object.values(BUTTON_SIZE)),
+    classNames: PropTypes.string,
 };
 
 Button.defaultProps = {
-  size: "medium",
-  classNames: undefined,
+    size: 'medium',
+    classNames: undefined,
 };
 
 export default Button;
--- src/containers/ChatSettings/index.jsx
+++ src/containers/ChatSettings/index.jsx
@@ -1,17 +1,15 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 
-import { ChatSettings } from "components";
+import { ChatSettings } from 'components';
 
 const ChatSettingsContainer = () => {
-  const [isPopupVisible, setPopupVisible] = useState(false);
+    const [isPopupVisible, setPopupVisible] = useState(false);
 
-  const togglePopup = () => {
-    setPopupVisible(!isPopupVisible);
-  };
+    const togglePopup = () => {
+        setPopupVisible(!isPopupVisible);
+    };
 
-  return (
-    <ChatSettings togglePopup={togglePopup} isPopupVisible={isPopupVisible} />
-  );
+    return <ChatSettings togglePopup={togglePopup} isPopupVisible={isPopupVisible} />;
 };
 
 export default ChatSettingsContainer;
